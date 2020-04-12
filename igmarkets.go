cc950455c9cd4abb5935a02f82fbf314ebfb8354
Markus van Laak <vanlaak@google.com>
2020-04-12 07:55:08 +0000

    added max parameter again

--- igmarkets.go
+++ igmarkets.go
@@ -537,8 +537,8 @@ func (ig *IGMarkets) GetPriceHistory(epic, resolution string, max int, from, to 
 		limitStr = fmt.Sprintf("&max=%d", max)
 	}
 
-	page := "&pageSize=100"
-	
+	page := "&max=1&pageSize=100"
+
 	req, err := http.NewRequest("GET", fmt.Sprintf("%s/gateway/deal/prices/%s?resolution=%s",
 		ig.APIURL, epic, resolution)+limitStr+page, bodyReq)
 	if err != nil {
