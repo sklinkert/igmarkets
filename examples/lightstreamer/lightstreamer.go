792d2e867cd3d52c95e1032ec4a5c6097b0d9ddc
Simon Klinkert <simon.klinkert@gmail.com>
2020-12-01 21:35:07 +0100

    add examples/lightstreamer/lightstreamer.go

---
+++ examples/lightstreamer/lightstreamer.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"github.com/sklinkert/igmarkets"
+	"github.com/lfritz/env"
+	log "github.com/sirupsen/logrus"
+	"time"
+)
+
+var conf struct {
+	igAPIURL               string
+	igIdentifier           string
+	igAPIKey               string
+	igPassword             string
+	igAccountID            string
+	instrument             string
+}
+
+func main() {
+	var e = env.New()
+	e.OptionalString("INSTRUMENT", &conf.instrument, "CS.D.EURUSD.MINI.IP", "instrument to trade")
+	e.OptionalString("IG_API_URL", &conf.igAPIURL, igmarkets.DemoAPIURL, "IG API URL")
+	e.OptionalString("IG_IDENTIFIER", &conf.igIdentifier, "", "IG Identifier")
+	e.OptionalString("IG_API_KEY", &conf.igAPIKey, "", "IG API key")
+	e.OptionalString("IG_PASSWORD", &conf.igPassword, "", "IG password")
+	e.OptionalString("IG_ACCOUNT", &conf.igAccountID, "", "IG account ID")
+	if err := e.Load(); err != nil {
+		log.WithError(err).Fatal("env loading failed")
+	}
+
+	for {
+		igHandle := igmarkets.New(conf.igAPIURL, conf.igAPIKey, conf.igAccountID, conf.igIdentifier, conf.igPassword, time.Second*30)
+		if err := igHandle.Login(); err != nil {
+			log.WithError(err).Error("new fialed")
+			return
+		}
+
+		tickChan := make(chan igmarkets.LightStreamerTick)
+		err := igHandle.OpenLightStreamerSubscription([]string{"CS.D.EURUSD.MINI.IP", "CS.D.BITCOIN.CFD.IP"}, tickChan)
+		if err != nil {
+			log.WithError(err).Error("open stream fialed")
+		}
+
+		for tick := range tickChan {
+			log.Infof("tick: %+v", tick)
+		}
+
+		log.Infof("Server closed stream, restarting...")
+	}
+}
