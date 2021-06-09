9de1c09f05d6fb2492b3e043aefadb55a365579b
Simon Klinkert <simon.klinkert@gmail.com>
2021-06-09 12:00:50 +0200

    Add cmd/lightstreamer as example

---
+++ cmd/lightstreamer/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	log "github.com/sirupsen/logrus"
+	"github.com/sklinkert/igmarkets"
+	"os"
+	"time"
+)
+
+func main() {
+	var (
+		url         = igmarkets.DemoAPIURL
+		apiKey      = os.Getenv("IG_API_KEY")
+		accountId   = os.Getenv("IG_ACCOUNT")
+		identifier  = os.Getenv("IG_IDENTIFIER")
+		password    = os.Getenv("IG_PASSWORD")
+		httpTimeout = time.Second * 10
+	)
+
+	var ig = igmarkets.New(url, apiKey, accountId, identifier, password, httpTimeout)
+	if err := ig.Login(); err != nil {
+		log.WithError(err).Fatal("Login failed")
+	}
+
+	for {
+		tickChan := make(chan igmarkets.LightStreamerTick)
+		err := ig.OpenLightStreamerSubscription([]string{"CS.D.BITCOIN.CFD.IP"}, tickChan)
+		if err != nil {
+			log.WithError(err).Error("Starting lightstreamer subscription failed")
+		}
+
+		for tick := range tickChan {
+			log.Infof("Tick: %+v", tick)
+		}
+
+		log.Info("Server closed stream, restarting...")
+	}
+}
